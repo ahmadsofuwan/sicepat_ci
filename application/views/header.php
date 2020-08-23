2224f3c620d6bfea47bd13e34e2206142d86ed21
ahmadsofuwan <69912778+ahmadsofuwan@users.noreply.github.com>
2020-08-24 00:41:20 +0700

    masukan js scener

--- application/views/header.php
+++ application/views/header.php
@@ -82,19 +82,17 @@
         </div>
       </li>
 
-      <!-- Nav Item - Utilities Collapse Menu -->
+      <!-- Nav Item -Mobile  Collapse Menu -->
       <li class="nav-item">
         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
           <i class="fas fa-fw fa-wrench"></i>
-          <span>Utilities</span>
+          <span>Mobile</span>
         </a>
         <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
           <div class="bg-white py-2 collapse-inner rounded">
-            <h6 class="collapse-header">Custom Utilities:</h6>
-            <a class="collapse-item" href="utilities-color.html">Colors</a>
-            <a class="collapse-item" href="utilities-border.html">Borders</a>
-            <a class="collapse-item" href="utilities-animation.html">Animations</a>
-            <a class="collapse-item" href="utilities-other.html">Other</a>
+            <h6 class="collapse-header">Mobile:</h6>
+            <a class="collapse-item" href="utilities-color.html">Scener</a>
+            <a class="collapse-item" href="utilities-border.html">Show</a>
           </div>
         </div>
       </li>
